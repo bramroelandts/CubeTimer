888eab5ddfc0be05e7d1f1feccb8c66aad8fd3de
PanikParty <97777507+PanikParty@users.noreply.github.com>
2023-09-24 19:10:22 -0700

    Capacitive Touch Support
    Added

--- CubeTimer.ino
+++ CubeTimer.ino
@@ -2,14 +2,18 @@
  * CubeTimer servers as a timer when solving the Rubik's cube.
  * See https://www.youtube.com/watch?v=0gvcZBQACMk for more.
  * 
- * Written by Bram Roelandts in October 2018.
+ * Basd on implementation from by Bram Roelandts in October 2018.
  */
 
 #include "LedControl.h"
+#include <FastCapacitiveSensor.h>
+
+FastCapacitiveSensor fastRightSensor;
+FastCapacitiveSensor fastLeftSensor;
 
 // MARK: - Define hardware setup
 LedControl lc=LedControl(12,11,10,1);
-// Buttons
+// Physical Buttons
 const int rightButtonPin = 8;
 const int leftButtonPin = 2;
 const int resetButtonPin = 7;
@@ -34,10 +38,22 @@ void setup() {
   printLCD(0, 0, 1);
   printLCD(0, 3, 4);
   printLCD(0, 6, 7);
+
+  // Setup the pins for each sensor
+  pinMode(A0, OUTPUT);
+  pinMode(A1, INPUT);
+
+  pinMode(A2, OUTPUT);
+  pinMode(A3, INPUT);
+  // initialize fastRightSensor
+  fastRightSensor.begin(A0, A1, 5.0, 8, 10, 0.2);
+  // initialize fastRightSensor
+  fastLeftSensor.begin(A2, A3, 5.0, 8, 10, 0.2);
   
   // Initialize the buttons
-  pinMode(rightButtonPin, INPUT_PULLUP);
-  pinMode(leftButtonPin, INPUT_PULLUP);
+  // Physical buttons
+  // pinMode(rightButtonPin, INPUT_PULLUP);
+  // pinMode(leftButtonPin, INPUT_PULLUP);
   pinMode(resetButtonPin, INPUT_PULLUP);
 }
 
@@ -46,35 +62,86 @@ void setup() {
 #define CHRONO_RUNNING 2
 #define CHRONO_STOPPED 3
 
+
+#define HIGH_LIMIT 300
+#define LOW_LIMIT 500
+
 // MARK: - Main loop
 void loop() {
-
+  static int counter = 0;
   // Fetch the latest button state
-  int rightButtonState = digitalRead(rightButtonPin);
-  int leftButtonState = digitalRead(leftButtonPin);
+  static int rightButtonState = HIGH; // = digitalRead(rightButtonPin);
+  
+  //long val = rightSensor.capacitiveSensor(30);
+  double rVal = fastRightSensor.touch();
+  if (rVal > LOW_LIMIT && rightButtonState != LOW) {
+    rightButtonState = LOW;
+    //Serial.print("rightSensorLOW: ");
+    //Serial.println(val);
+  } else if (rVal < HIGH_LIMIT && rightButtonState != HIGH) {
+    rightButtonState = HIGH;
+    //Serial.print("rightSensorHIGH: ");
+    //Serial.println(val);
+  }
+
+  static int leftButtonState = HIGH; //digitalRead(leftButtonPin);
+  double lVal = fastLeftSensor.touch();
+  if (lVal > LOW_LIMIT && leftButtonState != LOW) {
+    leftButtonState = LOW;
+    //Serial.print("leftSensorLOW: ");
+    //Serial.println(val);
+  } else if (lVal < HIGH_LIMIT && leftButtonState != HIGH) {
+    leftButtonState = HIGH;
+    //Serial.print("leftSensorHIGH: ");
+    //Serial.println(val);
+  }
+  Serial.print("Left:");
+  Serial.print(lVal);
+  Serial.print(",");
+  Serial.print("Right:");
+  Serial.println(rVal);
+
+
+
   // Determine whether one or multiple buttons were pressed
-  bool buttonsPressed = (rightButtonState == LOW) ? (leftButtonState == LOW) : false;
+  bool buttonsPressed =((rightButtonState == LOW) && (leftButtonState == LOW));// (rightButtonState == LOW) ? (leftButtonState == LOW) : false;
   bool oneButtonPressed = ((rightButtonState == LOW) || (leftButtonState == LOW));
 
-  static int chronoState = 0; // initial state
+  static int chronoState = CHRONO_CLEAR; // initial state
+
+  if (0 == counter%1000) {
+    //Serial.print ("IN_CHRONO_STATE: ");
+    //Serial.println (chronoState);        
+  }
   switch (chronoState) {
     case CHRONO_CLEAR:
     
       if (buttonsPressed) {
         
         chronoState = CHRONO_HOLD;
+        //Serial.print ("TO_CHRONO_STATE: ");
+        //Serial.println (chronoState);        
       }
      break;
     case CHRONO_HOLD:
+      // Buttons released
       if (!oneButtonPressed) {
+        //Serial.print ("Buttons: ");
+        //Serial.print (leftButtonState);
+        //Serial.print ("--");
+        //Serial.println (rightButtonState);
         chronoState = CHRONO_RUNNING;
         startTime = micros()/1000;
+        //Serial.print ("TO_CHRONO_STATE: ");
+        //Serial.println (chronoState);        
       }
       break;
     case CHRONO_RUNNING:
-      // Stop the chrono if any buttons; other than reset have been pressed
-      if (oneButtonPressed) {
+      // Stop the chrono if any buttons; other than reset have been pressed after more than 1000 milliseconds
+      if (oneButtonPressed && ((micros()/1000) - startTime > 1000) ) {
         chronoState = CHRONO_STOPPED;
+        //Serial.print ("TO_CHRONO_STATE: ");
+        //Serial.println (chronoState);        
       } else {
         updateTime();
       }
@@ -86,9 +153,16 @@ void loop() {
         // Zero the time
         startTime = micros()/1000;
         updateTime();
+        //Serial.print ("TO_CHRONO_STATE: ");
+        //Serial.println (chronoState);        
       }
       break;
   }
+  if (0 == counter%1000) {
+    //Serial.print ("OUT_CHRONO_STATE: ");
+    //Serial.println (chronoState);        
+  }
+  ++counter;
  
 }
 
